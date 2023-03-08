a9036e05f9ffe4f766ec13233ef9f823ae66cc97
applh.com <contact@applh.com>
2023-03-08 16:25:08 +0100

    v0.3.3 add app template / grid / cards / sortable

--- media/app.php
+++ media/app.php
@@ -22,18 +22,38 @@
                     <div class="uk-navbar-center">
 
                         <ul class="uk-navbar-nav">
-                            <li class="uk-active"><a href="#">Active</a></li>
+                            <li class="uk-active"><a href="#">Dashboard</a></li>
                             <li>
-                                <a href="#">Parent</a>
+                                <a href="#">Standard</a>
                                 <div class="uk-navbar-dropdown">
                                     <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li class="uk-active"><a href="#">Active</a></li>
+                                        <li class="uk-active"><a href="#">Dashboard</a></li>
                                         <li><a href="#">Item</a></li>
                                         <li><a href="#">Item</a></li>
                                     </ul>
                                 </div>
                             </li>
-                            <li><a href="#">Item</a></li>
+                            <li>
+                                <a href="#">Custom</a>
+                                <div class="uk-navbar-dropdown">
+                                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                                        <li class="uk-active"><a href="#">Dashboard</a></li>
+                                        <li><a href="#">Item</a></li>
+                                        <li><a href="#">Item</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="#">Expert</a>
+                                <div class="uk-navbar-dropdown">
+                                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                                        <li class="uk-active"><a href="#">Dashboard</a></li>
+                                        <li><a href="#">Item</a></li>
+                                        <li><a href="#">Item</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a href="#">Logout</a></li>
                         </ul>
 
                     </div>
@@ -47,19 +67,71 @@
             <div class="uk-navbar-container uk-width-1-6">
                 <div class="uk-container">
                     <ul class="uk-nav uk-nav-default">
-                        <li class="uk-active"><a href="#">Active</a></li>
-                        <li><a href="#">Item</a></li>
-                        <li><a href="#">Item</a></li>
-                        <li><a href="#offcanvas-usage" uk-toggle>Open</a></li>
+                        <li class="uk-active"><a href="#">Pages</a></li>
+                        <li><a href="#">Templates</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#offcanvas-usage" uk-toggle>Options</a></li>
                     </ul>
                 </div>
             </div>
             <div class="uk-container uk-width-5-6">
-                <div>{{ message }}</div>
+                <div class="uk-container uk-width-6-6">
+                    <h1>Pages</h1>
+                    <input type="range" v-model="max" min="0" max="1000" step="1">
+                    <input type="number" v-model="max" min="0" max="1000" step="1">
+                    <span> pages</span>
+                </div>
+                <div uk-grid uk-sortable>
+                    <div class="uk-card uk-card-small uk-card-default uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl">
+                        <div class="uk-card-header">
+                            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                <div class="uk-width-auto">
+                                    <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg" alt="Avatar">
+                                </div>
+                                <div class="uk-width-expand">
+                                    <h3 class="uk-card-title uk-margin-remove-bottom">Title</h3>
+                                    <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                        <div class="uk-card-footer">
+                            <a href="#" class="uk-button uk-button-text">Read more</a>
+                        </div>
+                    </div>
 
+                    <div v-for="n in (1 * max)" class="uk-card uk-card-small uk-card-default uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl">
+                        <div class="uk-card-header">
+                            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                <div class="uk-width-auto">
+                                    <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg" alt="Avatar">
+                                </div>
+                                <div class="uk-width-expand">
+                                    <h3 class="uk-card-title uk-margin-remove-bottom">Title ({{ n }})</h3>
+                                    <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                        <div class="uk-card-footer">
+                            <a href="#offcanvas-usage" uk-toggle class="uk-button uk-button-text">Read more</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="uk-container uk-width-6-6">
+                <span>{{ message }}</span>
                 <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-usage">Open</button>
                 <a href="#offcanvas-usage" uk-toggle>Open</a>
             </div>
+
         </div>
 
         <div id="offcanvas-usage" uk-offcanvas>
@@ -96,6 +168,7 @@
             template: '#appTemplate',
             data() {
                 return {
+                    max:100,
                     message: 'Hello Vue!'
                 }
             }
