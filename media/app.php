67f0bf8de0d35b8229123e3f404a010fc2247cc0
applh.com <contact@applh.com>
2023-03-08 19:38:37 +0100

    v0.3.6 add components

--- media/app.php
+++ media/app.php
@@ -21,7 +21,7 @@
 
                     <div class="uk-navbar-center">
 
-                        <ul class="uk-navbar-nav">
+                        <ul class="uk-navbar-nav" v-if="login_user">
                             <li class="uk-active"><a href="#" @click.prevent="panel='dashboard'">Dashboard</a></li>
                             <li>
                                 <a href="#" @click.prevent="panel='pages'">Standard</a>
@@ -53,7 +53,8 @@
                                     </ul>
                                 </div>
                             </li>
-                            <li><a href="#" @click.prevent="panel='login'">Logout</a></li>
+
+                            <li><a href="#" @click.prevent="login_user='';panel='login'">Logout</a></li>
                         </ul>
 
                     </div>
@@ -66,22 +67,20 @@
             <div class="uk-container uk-width-6-6">
                 <div class="uk-flex uk-flex-center">
                     <div class="uk-card uk-card-default uk-card-body">
-                        <h1>Login</h1>
+                        <h1>Welcome</h1>
                     </div>
                     <div class="uk-card uk-card-default uk-card-body uk-margin-left">
-                        <form>
+                        <form @submit.prevent="login_user=login_email;panel='dashboard'">
                             <fieldset class="uk-fieldset">
-
                                 <legend class="uk-legend">Login</legend>
-
                                 <div class="uk-margin">
-                                    <input class="uk-input" type="email" placeholder="email" aria-label="email">
+                                    <input class="uk-input" type="email" required placeholder="email" aria-label="email" v-model="login_email">
                                 </div>
                                 <div class="uk-margin">
-                                    <input class="uk-input" type="password" placeholder="password" aria-label="password">
+                                    <input class="uk-input" type="password" required placeholder="password" aria-label="password">
                                 </div>
                                 <div class="uk-margin">
-                                    <button class="uk-button uk-button-primary">Login</button>
+                                    <button type="submit" class="uk-button uk-button-primary">Login</button>
                                 </div>
                             </fieldset>
                         </form>
@@ -95,83 +94,22 @@
         </div>
 
         <div uk-grid v-if="panel=='dashboard'">
-            <div class="uk-container uk-width-6-6">
-                <div class="uk-flex uk-flex-center" uk-sortable>
-                    <div class="uk-card uk-card-default uk-card-body uk-width-1-2">
-                        <h1>Dashboard</h1>
-                    </div>
-                    <div class="uk-card uk-card-default uk-card-body uk-margin-left uk-width-1-2">Item 2</div>
-                    <div class="uk-card uk-card-default uk-card-body uk-margin-left uk-width-1-2">Item 3</div>
-                </div>
+            <div class="uk-navbar-container uk-width-1-6">
+                <menu-sidebar />
+            </div>
+            <div class="uk-width-5-6">
+                <dashboard-main />
             </div>
         </div>
 
         <div uk-grid v-else-if="panel=='pages'">
 
             <div class="uk-navbar-container uk-width-1-6">
-                <div class="uk-container">
-                    <ul class="uk-nav uk-nav-default">
-                        <li class="uk-active"><a href="#">Pages</a></li>
-                        <li><a href="#">Templates</a></li>
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#offcanvas-usage" uk-toggle>Options</a></li>
-                    </ul>
-                </div>
+                <menu-sidebar />
             </div>
             <div class="uk-container uk-width-5-6">
-                <div class="uk-container uk-width-6-6">
-                    <h1>Pages</h1>
-                    <input type="range" v-model="max" min="0" max="1000" step="1">
-                    <input type="number" v-model="max" min="0" max="1000" step="1">
-                    <span> pages</span>
-                </div>
-                <div uk-grid uk-sortable>
-
-                    <div v-for="n in (1 * max)" class="uk-card uk-card-small uk-card-default uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl">
-                        <div class="uk-card-header">
-                            <div class="uk-grid-small uk-flex-middle" uk-grid>
-                                <div class="uk-width-auto">
-                                    <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg" alt="Avatar">
-                                </div>
-                                <div class="uk-width-expand">
-                                    <h3 class="uk-card-title uk-margin-remove-bottom">Title ({{ n }})</h3>
-                                    <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="uk-card-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                            <a href="#offcanvas-usage" uk-toggle class="uk-button uk-button-text">Read more</a>
-                        </div>
-                        <div class="uk-card-footer">
-                            <div>
-                                <div class="uk-button-group">
-                                    <button class="uk-button uk-button-small uk-button-secondary" uk-toggle="target: #modal-copy">copy</button>
-                                    <button class="uk-button uk-button-small uk-button-primary" uk-toggle="target: #modal-update">update</button>
-                                    <button class="uk-button uk-button-small uk-button-danger" uk-toggle="target: #modal-delete">delete</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+                <pages-main />
             </div>
-
-            <div class="uk-container uk-width-6-6">
-                <div class="uk-flex uk-flex-center" uk-sortable>
-                    <div class="uk-card uk-card-default uk-card-body uk-width-1-2">
-                        <span>{{ message }}</span>
-                    </div>
-                    <div class="uk-card uk-card-default uk-card-body uk-margin-left uk-width-1-2">
-                        <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-usage">Open</button>
-                    </div>
-                    <div class="uk-card uk-card-default uk-card-body uk-margin-left uk-width-1-2">
-                        <a href="#offcanvas-usage" uk-toggle>Open</a>
-                    </div>
-                </div>
-            </div>
-
         </div>
 
         <div id="modal-delete" uk-modal>
@@ -180,7 +118,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p class="uk-text-right">
                     <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
-                    <button class="uk-button uk-button-primary" type="button">Delete Item</button>
+                    <button class="uk-button uk-button-danger" type="button">Delete Item</button>
                 </p>
             </div>
         </div>
@@ -285,21 +223,138 @@
 
     </template>
 
+    <template id="sidebarTemplate">
+        <div class="uk-container">
+            <ul class="uk-nav uk-nav-default">
+                <li><a href="#">Media</a></li>
+                <li><a href="#">Articles</a></li>
+                <li class="uk-active"><a href="#">Pages</a></li>
+                <li><a href="#">Templates</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#offcanvas-usage" uk-toggle>Options</a></li>
+            </ul>
+        </div>
+
+    </template>
+
+    <template id="DashboardMain">
+        <div class="uk-flex-center" uk-grid uk-sortable>
+            <div class="uk-card uk-card-default uk-card-body uk-width-6-6">
+                <h1>Dashboard</h1>
+                <em>Welcome {{ login_user }}</em>
+            </div>
+            <div v-for="p in 16" class="uk-card uk-card-default uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg" alt="Avatar">
+                    <h3 class="uk-card-title">Item {{ p }}</h3>
+                </div>
+                <div class="uk-card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <a href="#offcanvas-usage" uk-toggle class="uk-button uk-button-text">Read more</a>
+                </div>
+            </div>
+        </div>
+
+    </template>
+
+    <template id="PagesMain">
+        <div class="uk-container uk-width-6-6">
+            <h1>Pages</h1>
+            <input type="range" v-model="max" min="0" max="1000" step="1">
+            <input type="number" v-model="max" min="0" max="1000" step="1">
+            <span> pages</span>
+        </div>
+
+        <div uk-grid uk-sortable>
+            <div v-for="n in (1 * max)" class="uk-card uk-card-small uk-card-default uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl">
+                <div class="uk-card-header">
+                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg" alt="Avatar">
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="uk-card-title uk-margin-remove-bottom">Title ({{ n }})</h3>
+                            <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <a href="#offcanvas-usage" uk-toggle class="uk-button uk-button-text">Read more</a>
+                </div>
+                <div class="uk-card-footer">
+                    <div>
+                        <div class="uk-button-group">
+                            <button class="uk-button uk-button-small uk-button-secondary" uk-toggle="target: #modal-copy"><span title="copy" class="" uk-icon="icon: copy; ratio: 1"></span></button>
+                            <button class="uk-button uk-button-small uk-button-primary" uk-toggle="target: #modal-update"><span title="update" class="" uk-icon="icon: file-edit; ratio: 1"></span></button>
+                            <button class="uk-button uk-button-small uk-button-danger" uk-toggle="target: #modal-delete"><span title="delete" class="" uk-icon="icon: trash; ratio: 1"></span></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="uk-container uk-width-6-6">
+            <div class="uk-flex-center" uk-grid uk-sortable>
+                <div class="uk-card uk-card-default uk-card-body uk-width-6-6">
+                    <span>{{ message }}</span>
+                </div>
+                <div class="uk-card uk-card-default uk-card-body uk-width-1-4">
+                    <span>{{ message }}</span>
+                </div>
+                <div class="uk-card uk-card-default uk-card-body uk-width-1-4">
+                    <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-usage">Open</button>
+                </div>
+                <div class="uk-card uk-card-default uk-card-body uk-width-1-4">
+                    <a href="#offcanvas-usage" uk-toggle>Open</a>
+                </div>
+                <div class="uk-card uk-card-default uk-card-body uk-width-1-4">
+                    <a href="#offcanvas-usage" uk-toggle>Open</a>
+                </div>
+            </div>
+        </div>
+    </template>
+
     <script type="module">
         import {
             createApp
         } from './vue.esm-browser.prod.js'
 
-        createApp({
+        let app = createApp({
             template: '#appTemplate',
             data() {
                 return {
-                    panel: 'login',
+                    login_email: '',
+                    login_user: '',
+                    panel: 'login'
+                }
+            }
+        })
+
+        app.component('MenuSidebar', {
+            template: '#sidebarTemplate',
+            data() {
+                return {}
+            }
+        })
+
+        app.component('DashboardMain', {
+            template: '#DashboardMain',
+            data() {
+                return {}
+            }
+        })
+        app.component('PagesMain', {
+            template: '#PagesMain',
+            data() {
+                return {
                     max: 100,
                     message: 'Hello Vue!'
                 }
             }
-        }).mount('#app')
+        })
+
+        app.mount('#app')
     </script>
 
 </body>
